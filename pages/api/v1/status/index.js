3a6768f11b686c0f8299f54563885b1b6da26945
Eduardo Maines <edumaines27@gmail.com>
2025-03-29 21:45:11 -0300

    adiciona os arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function staus(request, response) {
+import database from "../../../../infra/database.js";
+
+async function staus(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({ status: "teste do send á" });
 }
 
